5d07259793b29f9641490abceac360230dba8c01
RocKIn0X <inman27@hotmail.com>
2017-01-31 05:23:11 +0700

    Fix dart

--- Week1/darts.cpp
+++ Week1/darts.cpp
@@ -3,31 +3,32 @@
 
 using namespace std;
 
-int getScore(int r) {
-  if (r <= 2) {
-    return 5;
-  } else if (r <= 4) {
-    return 4;
-  } else if (r <= 6) {
-    return 3;
-  } else if (r <= 8) {
-    return 2;
-  } else if (r <= 10) {
-    return 1;
-  }
+float getScore(float r) {
+   if (r <= 2) {
+     return 5;
+   } else if (r <= 4) {
+     return 4;
+   } else if (r <= 6) {
+     return 3;
+   } else if (r <= 8) {
+     return 2;
+   } else if (r <= 10) {
+     return 1;
+   }
 
-  return 0;
+   return 0;
 }
 
 int main() {
-  int n, x, y, r, score;
+  int n, x, y, score;
+  float r;
   score = 0;
 
   cin >> n;
 
   for (int i = 0; i < n; i++) {
     cin >> x >> y;
-    r = x + y;
+    r = (float) sqrt((x * x) + (y * y));
     score += getScore(r);
   }
 
